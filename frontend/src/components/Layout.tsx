1964f76f74260f03d095125f2d33f4cf07dabdf3
Simon Franken <mail@simon-franken.de>
2026-03-13 17:07:32 +0000

    fix: add bg-gray-50 back to Layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { TimerWidget } from './TimerWidget';
 
 export function Layout() {
   return (
-    <div className="h-[100vh] w-[100vw] flex flex-col">
+    <div className="h-[100vh] w-[100vw] flex flex-col bg-gray-50">
       <Navbar />
       <main className="pt-4 pb-8 grow overflow-auto">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
